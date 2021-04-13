69511433c9bec6321e0a57be2d1c8ca934728d44
vivomo <929992114@qq.com>
2021-04-14 00:26:27 +0800

    battle map && city style

--- src/components/battleground/Battleground.tsx
+++ src/components/battleground/Battleground.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import BattleMap from '../battle-map';
 
+import BattleMap from '../battle-map';
+import City from '../city';
 import './style.scss';
 
 const Battleground = () => {
     return (
         <div className="battleground">
+            <City/>
             <BattleMap/>
+            <City/>
         </div>
     );
 };
---
+++ src/components/city/City.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './style.scss';
+
+interface IProps {
+    name?: string;
+    initHP?: number;
+    HP?: number;
+}
+
+const City = ({name = '主城', HP = 300}: IProps) => {
+    return (
+        <div className="battle-city">
+            <div>{name}</div>
+            <div>HP: {HP}</div>
+        </div>
+    );
+};
+
+export default City;
